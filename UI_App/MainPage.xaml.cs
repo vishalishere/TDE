6657d9b8c05c3d386914bfb800cfef2b24c8ec75
ebragge <ebragge@live.com>
2015-11-11 18:40:19 +0200

    11112015 - 18:40

--- UI_App/MainPage.xaml.cs
+++ UI_App/MainPage.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Shapes;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -143,7 +132,6 @@ namespace UI_App
 
         private void ResetEngine(uint counter)
         {
-            startCounter = 0;
             engine.Finish();
             engine = null;
             EmptyTexts("RESETTING");
