92ddbdeb45452790ce84fef17a91c7acb8992619
Eero Bragge <ebragge@live.com>
2015-12-04 15:57:55 +0200

    04122015 - 15:57

--- LibAccess/Properties/AssemblyInfo.cs
+++ LibAccess/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AccessData")]
+[assembly: AssemblyTitle("LibAccess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AccessData")]
+[assembly: AssemblyProduct("LibAccess")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
