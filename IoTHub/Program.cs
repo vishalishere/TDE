7c4265e284e8fc89983e07b574e2223eac139549
Eero Bragge <ebragge@live.com>
2015-12-07 10:51:36 +0200

    07122015 - 10:51

--- IoTHub/Program.cs
+++ IoTHub/Program.cs
@@ -1,6 +1,7 @@
 ﻿//#define CREATE_NEW_DEVICE
 
 using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ServiceBus.Messaging;
@@ -20,12 +21,24 @@ namespace IoTHub
 
     class Program
     {
+        static string path = @"c:\temp\audiodata.txt";
 #if CREATE_NEW_DEVICE
         static RegistryManager registryManager;
 #else
         static string iotHubD2cEndpoint = "messages/events";
         static EventHubClient eventHubClient;
 #endif
+
+        private static void SaveQueue(System.Collections.Generic.Queue<string> queue)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                while (queue.Count > 0)
+                { 
+                    sw.WriteLine(queue.Dequeue());
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -35,6 +48,15 @@ namespace IoTHub
             Console.ReadLine();
 #else
             Console.WriteLine("Receive messages\n");
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(DateTime.UtcNow.ToString());
+                }
+            }
+
             eventHubClient = EventHubClient.CreateFromConnectionString(Access.ConnectionString, iotHubD2cEndpoint);
 
             var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
@@ -68,6 +90,8 @@ namespace IoTHub
 #else
         private async static Task ReceiveMessagesFromDeviceAsync(string partition)
         {
+            System.Collections.Generic.Queue<string> queue = new System.Collections.Generic.Queue<string>();
+
             var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.Now);
             while (true)
             {
@@ -75,6 +99,8 @@ namespace IoTHub
                 if (eventData == null) continue;
 
                 string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                queue.Enqueue(data);
+                if (queue.Count > 100) SaveQueue(queue);
                 Console.WriteLine(string.Format("Message: '{0}'", data));
             }
         }
